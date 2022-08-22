4a12269084ed1139d873da7f6b9d10a6a97f4e95
Thogan24 <thogan1924@gmail.com>
2022-08-22 11:38:25 -0400

    Added a way for the blogs to actually be stored and opened

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Import statements
 const express = require ("express");
 const path = require ("path");
-const bodyParser = require ("body-parser")
+const bodyParser = require ("body-parser");
+const { Console } = require("console");
 
 // Setting up the project
 const app = express();
@@ -10,27 +11,42 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+var blogs = {};
 
 // Manage Routes
 app.get("/", function(req, res){
-    return res.render("index")
+    var blogNames = Object.keys(blogs);
+    console.log(blogNames);
+    return res.render("index", {blogs: blogNames, statusMessage: req.query.status});
 })
 
 app.get("/about", function(req, res){
-    return res.render("about")
+    return res.render("about");
 })
 
 app.get("/newblog", function(req, res){
-    return res.render("newblog")
+    return res.render("newblog");
+})
+
+app.get("/blog/:blogname", function(req, res){
+    console.log(req.params.blogname)
+    
+    return res.render("blog", {name: req.params.blogname, text: blogs[req.params.blogname]});
 })
 
 app.post("/newblog", function(req, res){
+    var blogname = req.body.blogname;
     var blogtext = req.body.blogtext;
-    console.log(blogtext);
-    return res.render("newblog")
+    if (blogs[blogname] != undefined){
+        return res.redirect("/?status=Error: Name already exists")
+    }
+    blogs[blogname] = blogtext;
+    console.log(blogs);
+    return res.redirect("/?status=Blog Successfully Created")
+
 })
 
 // Listen on port 8000
 app.listen(8000, ()=>{
-    console.log("app started")
+    console.log("app started");
 })
